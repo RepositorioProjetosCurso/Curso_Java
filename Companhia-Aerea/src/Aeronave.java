f79f40e6c07859c0856db3bec413dace4a49e579
RepositorioCursos <repositoriocursosgit@gmail.com>
2024-07-19 16:02:43 -0300

    Adicionando Funcionalidades as Aeronaves

--- Companhia-Aerea/src/Aeronave.java
+++ Companhia-Aerea/src/Aeronave.java
@@ -7,4 +7,21 @@ public class Aeronave {
     int calcularAssentosDisponiveis() {
         return totalDeAssentos - assentosReservados;
     }
+
+    void reservarAssentos(int numeroDeAssentos) {
+        if (ativo) {
+            assentosReservados += numeroDeAssentos;
+            System.out.printf("Foram reservados %d assentos.%n", assentosReservados);
+        } else {
+            System.out.println("Aeronave desativada");
+        }
+    }
+
+    void desativar() {
+        ativo = false;
+    }
+
+    void ativar() {
+        ativo = true;
+    }
 }
--- Companhia-Aerea/src/Principal.java
+++ Companhia-Aerea/src/Principal.java
@@ -4,6 +4,20 @@ public class Principal {
         Aeronave aviaoGol = new Aeronave();
         aviaoGol.totalDeAssentos = 100;
 
+        aviaoGol.desativar();
+        aviaoGol.ativar();
+
+        aviaoGol.reservarAssentos(10);
+
         System.out.printf("GOL (%s): %d assentos disponiveis%n", aviaoGol.ativo ? "Ativo" : "Inativo", aviaoGol.calcularAssentosDisponiveis());
+
+        Aeronave aviaoLatam = new Aeronave();
+        aviaoLatam.totalDeAssentos = 120;
+
+        aviaoLatam.desativar();
+
+        aviaoLatam.reservarAssentos(30);
+
+        System.out.printf("LATAM (%s): %d assentos disponiveis%n%n", aviaoLatam.ativo ? "Ativo" : "Inativo", aviaoLatam.calcularAssentosDisponiveis());
     }
 }
