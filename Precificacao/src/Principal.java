cc1925217847491f4291c3390cd207dc5a2833c4
RepositorioCursos <repositoriocursosgit@gmail.com>
2024-07-20 14:41:55 -0300

    Criando um metodo para uso da função

--- Precificacao/src/Principal.java
+++ Precificacao/src/Principal.java
@@ -1,6 +1,11 @@
 public class Principal {
 
     public static void main(String[] args) {
+        double precoCompraFornecedor = 140;
 
+        ServicoDePrecificacao servicoDePrecificacao = new ServicoDePrecificacao();
+        double precoVenda = servicoDePrecificacao.calcularPrecoDeVenda(precoCompraFornecedor);
+
+        System.out.printf("Preço: %.2f%n", precoVenda);
     }
 }
--- Precificacao/src/ServicoDePrecificacao.java
+++ Precificacao/src/ServicoDePrecificacao.java
@@ -1,3 +1,7 @@
 public class ServicoDePrecificacao {
 
+    double calcularPrecoDeVenda(double precoCusto) {
+
+        return precoCusto * 1.20;
+    }
 }
