5eff4f792199966ab79e6baaa72daa883bf22a8f
RepositorioCursos <repositoriocursosgit@gmail.com>
2024-07-24 16:30:51 -0300

    Criando o cadastro do visitante e dizendo o tempo de acesso

--- Portaria/src/Principal.java
+++ Portaria/src/Principal.java
@@ -4,14 +4,16 @@ public class Principal {
 
         Visitante visitante1 = new Visitante();
         visitante1.nome = "Pedro";
-        visitante1.idade = 15;
+        visitante1.idade = 19;
 
         if (visitante1.possuiAcessoRestritoPorIdade()) {
-            System.out.printf("Acesso não liberado para menores de %d", Visitante.IDADE_MINIMA_PARA_ACESSO);
+            System.out.printf("Acesso não liberado para menores de %d%n", Visitante.IDADE_MINIMA_PARA_ACESSO);
         } else {
             System.out.println("Acesso liberado");
             System.out.println("Teste do Pull");
-            
+
+            CadastroPortaria cadastroPortaria = new CadastroPortaria();
+            cadastroPortaria.cadastrar(visitante1, 5);
         }
     }
 }
