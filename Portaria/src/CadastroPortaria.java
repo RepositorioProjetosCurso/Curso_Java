877b31028e96c0fb68a3183ee7067402b661c723
RepositorioCursos <repositoriocursosgit@gmail.com>
2024-07-25 15:08:05 -0300

    Adicionando o tempo de acesso padrão para visitantes que não informam o tempo de acesso

--- Portaria/src/CadastroPortaria.java
+++ Portaria/src/CadastroPortaria.java
@@ -1,5 +1,11 @@
 public class CadastroPortaria {
 
+    static final int TEMPO_DE_EXPIRACAO_PADRAO_EM_MESES = 1;
+
+    void cadastrar(final Visitante visitante) {
+        this.cadastrar(visitante, TEMPO_DE_EXPIRACAO_PADRAO_EM_MESES);
+    }
+
     void cadastrar(Visitante visitante, final int tempoExpiracaoEmMeses) {
         final int tempoExpiracaoEmDias = tempoExpiracaoEmMeses * 30;
 
--- Portaria/src/Principal.java
+++ Portaria/src/Principal.java
@@ -6,6 +6,10 @@ public class Principal {
         visitante1.nome = "Pedro";
         visitante1.idade = 19;
 
+        Visitante visitante2 = new Visitante();
+        visitante2.nome = "Claudia";
+        visitante2.idade = 35;
+
         if (visitante1.possuiAcessoRestritoPorIdade()) {
             System.out.printf("Acesso não liberado para menores de %d%n", Visitante.IDADE_MINIMA_PARA_ACESSO);
         } else {
@@ -13,7 +17,8 @@ public class Principal {
             System.out.println("Teste do Pull");
 
             CadastroPortaria cadastroPortaria = new CadastroPortaria();
-            cadastroPortaria.cadastrar(visitante1, 5);
+            cadastroPortaria.cadastrar(visitante1);
+            cadastroPortaria.cadastrar(visitante2, 3);
         }
     }
 }
