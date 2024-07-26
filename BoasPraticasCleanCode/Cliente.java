106c5a58e5fc472870c2e49fcdfd5915002c1667
RepositorioCursos <repositoriocursosgit@gmail.com>
2024-07-26 15:12:44 -0300

    Adicionando a pasta boas práticas, e mudando outras para pasta de OO

---
+++ BoasPraticasCleanCode/Cliente.java
@@ -0,0 +1,17 @@
+public class Cliente {
+
+    double calcularLimiteCredito() {
+        double limiteCredito = faturamentoAnual * TAXA_LIMITE_CREDITO;
+
+        if (limiteCredito > LIMITE_CREDITO_MINIMO) {
+            limiteCredito = LIMITE_CREDITO_MINIMO;
+        }
+
+        return limiteCredito;
+    }
+
+    String razaoSocial;
+    static final double TAXA_LIMITE_CREDITO = 0.10;
+    static final double LIMITE_CREDITO_MINIMO = 10_000;
+    double faturamentoAnual;q
+}
