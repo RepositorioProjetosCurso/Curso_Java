c90757bf14204f7b2d6fe3883d864592828e918f
RepositorioCursos <repositoriocursosgit@gmail.com>
2024-07-30 14:44:04 -0300

    Colocando modulo na pasta escola, e adicionando funcionalidades

--- 3. Arrays/TurmaEscola/src/Main.java
+++ 3. Arrays/TurmaEscola/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Turma turmaB = new Turma();
         turmaB.identificacaoTurma = "Maternal B";
         turmaB.nomeProfessora = "Claudia";
-        turmaB.alunos = new Aluno[5];
+        turmaB.alunos = new Aluno[3];
 
 //        1 Forma de criar uma aluno em array
         turmaB.alunos[0] = new Aluno();
@@ -17,5 +17,17 @@ public class Main {
         aluno1.nome = "Julia Maria";
         aluno1.idade = 4;
         turmaB.alunos[1] = aluno1;
+
+        turmaB.imprimirListaDeAlunos();
+
+//        for (int i = 0; i < turmaB.alunos.length; i++) {
+//            Aluno aluno = turmaB.alunos[i];
+//
+//            if (aluno != null) {
+//                System.out.printf("%d - %s (%d anos)%n", i, aluno.nome, aluno.idade);
+//            } else {
+//                System.out.printf("%d está vago%n", i);
+//            }
+//        }
     }
 }
--- 3. Arrays/TurmaEscola/src/Turma.java
+++ 3. Arrays/TurmaEscola/src/Turma.java
@@ -3,4 +3,14 @@ public class Turma {
     String identificacaoTurma;
     String nomeProfessora;
     Aluno[] alunos;
+    
+    void imprimirListaDeAlunos() {
+        for (Aluno aluno : alunos) {
+            if (aluno != null) {
+                System.out.printf("%s (%d anos)%n", aluno.nome, aluno.idade);
+            } else {
+                System.out.println("Vago");
+            }
+        }
+    }
 }
